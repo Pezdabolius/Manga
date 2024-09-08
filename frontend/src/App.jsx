f47af93ea4bccbf90f0ac31f821ddb4002e9390f
Sultan <tamaevsultan@gmail.com>
2024-09-08 18:49:04 +0600

    front: vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header"
+import Home from "./pages/home/Home"
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Home/>
+    </div>
+      
+  )
+}
+
+export default App
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import s from './Home.module.scss'
+
+const Home = () => {
+  return (
+      <div className={s.home}>
+         <div className={s.container}>
+            <div className={s.content}>
+               hello
+            </div>
+         </div>
+      </div>
+  )
+}
+
+export default Home
