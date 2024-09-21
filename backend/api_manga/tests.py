4ba2c38889024a0559a61e36eee74b8a0ced6433
Pezdabolius <naskafe.muxinov@yandex.ru>
2024-09-21 22:49:50 +0300

    Testing

--- backend/api_manga/tests.py
+++ backend/api_manga/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Tag, Author, Publisher, Artist, Manga, \
+    Genre
 
-# Create your tests here.
+
+class TRGTest(APITestCase):
+    model = Tag
+    url = 'tags'
+    data = {
+        'name': 'Cold Weapons'
+    }
+
+    def get_url(self):
+        return reverse(self.url)
+
+    def test_trg_list(self):
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_trg_post(self):
+        response = self.client.post(self.get_url(), data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Tag.objects.filter(name=self.data['name']).exists())
+
+
+class AAPTest(APITestCase):
+    model = Author
+    url_list = 'author_list'
+    url_detail = 'author_detail'
+    data = {'name': 'Yukinobu Tatsu',
+            'description': 'To start, Tatsu was asked about the "detail" that '
+                           'went into the drawings of the manga'
+            }
+
+    def get_url(self, url, *args):
+        return reverse(url, args=args)
+
+    def setUp(self):
+        self.author = Author.objects.create(**self.data)
+
+    def test_app_list(self):
+        response = self.client.get(self.get_url(self.url_list))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_app_post(self):
+        response = self.client.post(self.get_url(self.url_list), data=self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Author.objects.filter(description=self.data['description']).exists())
--- backend/api_manga/urls.py
+++ backend/api_manga/urls.py
@@ -2,19 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/manga_list/', views.manga_list),
-    path('api/manga_detail/<pk>/', views.manga_detail),
-    path('api/manga_types/', views.TypeAPI.as_view()),
-    path('api/manga_status/', views.StatusAPI.as_view()),
-    path('api/manga_rating/', views.RatingAPI.as_view()),
-    path('api/author_list/', views.AuthorAPI.as_view()),
-    path('api/author_detail/<pk>/', views.AuthorAPI.as_view()),
-    path('api/artists_list/', views.ArtistAPI.as_view()),
-    path('api/artist_detail/<pk>/', views.ArtistAPI.as_view()),
-    path('api/publishers_list/', views.PublisherAPI.as_view()),
-    path('api/publisher_detail/<pk>/', views.PublisherAPI.as_view()),
-    path('api/tags_list/', views.TagAPI.as_view()),
-    path('api/genres_list/', views.GenreAPI.as_view()),
-    path('api/release_list/', views.ReleaseAPI.as_view()),
-    path('api/chapter_detail/<pk>/', views.chapter_detail),
+    path('api/manga/', views.manga_list, name='manga_list'),
+    path('api/manga/<pk>/', views.manga_detail, name='manga_detail'),
+    path('api/types/', views.TypeAPI.as_view()),
+    path('api/status/', views.StatusAPI.as_view()),
+    path('api/rating/', views.RatingAPI.as_view()),
+
+    path('api/authors/', views.AuthorAPI.as_view(), name='author_list'),
+    path('api/authors/<pk>/', views.AuthorAPI.as_view(), name='author_detail'),
+
+    path('api/artists/', views.ArtistAPI.as_view()),
+    path('api/artists/<pk>/', views.ArtistAPI.as_view()),
+
+    path('api/publishers/', views.PublisherAPI.as_view()),
+    path('api/publishers/<pk>/', views.PublisherAPI.as_view()),
+
+    path('api/tags/', views.TagAPI.as_view(), name='tags'),
+    path('api/genres/', views.GenreAPI.as_view()),
+    path('api/release/', views.ReleaseAPI.as_view()),
+
+    path('api/chapter/<pk>/', views.chapter_detail, name='chapter'),
 ]
